d3a8b0b658e48f152cb9d23c15a370ef51dac767
Robert Fratto <robertfratto@gmail.com>
2022-02-18 18:26:50 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/rfratto/lokiecho
+
+go 1.17
+
+require github.com/prometheus/prometheus v1.8.2-0.20211210131051-6f3e664ae712
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/goleak v1.1.12 // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/prometheus/prometheus/promql/parser"
+)
+
+func main() {
+	var (
+		lokiUrl   string
+		username  string
+		password  string
+		rawLabels string
+	)
+
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	fs.StringVar(&lokiUrl, "addr", "", "Server address. Defaults to $LOKI_ADDR if not set.")
+	fs.StringVar(&username, "username", "", "Username for basic auth. Defaults to $LOKI_USERNAME if not set.")
+	fs.StringVar(&password, "password", "", "Password for basic auth. Defaults to $LOKI_PASSWORD if not set.")
+	fs.StringVar(&rawLabels, "labels", `{job="lokiecho"}`, `Labels to inject for logs. i.e., {app="shell"}`)
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		abort("error: could not parse flags: %s", err)
+	}
+
+	lokiUrl = stringOrDefault(lokiUrl, os.Getenv("LOKI_ADDR"))
+	username = stringOrDefault(username, os.Getenv("LOKI_USERNAME"))
+	password = stringOrDefault(password, os.Getenv("LOKI_PASSWORD"))
+
+	if lokiUrl == "" {
+		abort("error: --addr must be provided or $LOKI_ADDR must be set")
+	}
+	if (username != "" || password != "") && (username == "" || password == "") {
+		abort("error: username and password must be set together, but one is unset")
+	}
+
+	labels, err := parser.ParseMetric(rawLabels)
+	if err != nil {
+		abort("error: could not parse -labels: %s", err)
+	}
+
+	pushReq := pushRequest{
+		Streams: []pushStream{{
+			Stream: labels.Map(),
+			Values: [][]string{{
+				fmt.Sprintf("%d", time.Now().UnixNano()),
+				strings.Join(fs.Args(), " "),
+			}},
+		}},
+	}
+
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(&pushReq); err != nil {
+		abort("error: could not build request: %s", err)
+	}
+
+	parsedUrl, err := url.Parse(lokiUrl)
+	if err != nil {
+		abort("error: invalid url %s: %s", lokiUrl, err)
+	}
+	parsedUrl.Path = path.Join(parsedUrl.Path, "/loki/api/v1/push")
+
+	req, err := http.NewRequest(http.MethodPost, parsedUrl.String(), bytes.NewReader(buf.Bytes()))
+	if err != nil {
+		abort("error: invalid http request: %s", err)
+	}
+	if username != "" || password != "" {
+		req.SetBasicAuth(username, password)
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		abort("error: failed to perform http request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode/100 != 2 {
+		bb, _ := io.ReadAll(resp.Body)
+		abort("error: response %s from loki: %s", resp.Status, string(bb))
+	}
+}
+
+func abort(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg, args...)
+	os.Exit(1)
+}
+
+func stringOrDefault(value string, def string) string {
+	if value != "" {
+		return value
+	}
+	return def
+}
+
+type pushRequest struct {
+	Streams []pushStream `json:"streams"`
+}
+
+type pushStream struct {
+	Stream map[string]string `json:"stream"`
+	Values [][]string        `json:"values"`
+}
