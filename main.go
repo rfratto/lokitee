016d246b5ef4cc7301c1d97ccfb6996b2e14f1fe
Kevin Minehart <kmineh0151@gmail.com>
2022-03-09 20:25:49 -0600

    exit whenever signal is sent

--- main.go
+++ main.go
@@ -47,8 +47,9 @@ func main() {
 	go func() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt)
-		<-c
+		sig := <-c
 		time.Sleep(time.Duration(interruptWaitSeconds) * time.Second)
+		abort("receiving signal '%s'", sig.String())
 	}()
 
 	lokiUrl = stringOrDefault(lokiUrl, os.Getenv("LOKI_ADDR"))
